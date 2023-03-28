9779062ad768c72884b18a4949296f127876fa4f
dobronrav3 <121180965+dobronrav3@users.noreply.github.com>
2023-03-28 17:04:38 +0300

    Update settings.py

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#!fs2$eywm+igidw=@41=ol5%x3x@tp9gm8d$v1l$x)n1+7cy#'
+#SECRET_KEY = 'django-insecure-#!fs2$eywm+igidw=@41=ol5%x3x@tp9gm8d$v1l$x)n1+7cy#'
 
-#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-#!fs2$eywm+igidw=@41=ol5%x3x@tp9gm8d$v1l$x)n1+7cy#')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-#!fs2$eywm+igidw=@41=ol5%x3x@tp9gm8d$v1l$x)n1+7cy#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+#DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
